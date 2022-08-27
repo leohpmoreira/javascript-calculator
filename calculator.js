bf21ea159e753a6c04e3142dd2a1ec19dcc02568
leohpmoreira <90646838+leohpmoreira@users.noreply.github.com>
2022-08-27 20:22:35 -0300

    Add instructions button

--- calculator.js
+++ calculator.js
@@ -106,6 +106,9 @@ const ceButton = document.querySelector('[data-ce]')
 const cButton = document.querySelector('[data-c]')
 const curOperandTextElement = document.querySelector('[data-cur]')
 const prevOperandTextElement = document.querySelector('[data-prev]')
+const instrButton = document.querySelector('[data-instruction]');
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
 
 const calculator = new Calculator(curOperandTextElement, prevOperandTextElement)
 
@@ -157,3 +160,19 @@ window.addEventListener('keypress', (e) => {
         }
     }
 })
+
+instrButton.addEventListener('click', () => {
+    modal.style.display = 'block';
+})
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
